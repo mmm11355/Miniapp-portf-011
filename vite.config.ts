2f8216621a12980fae5821559cdb0d09bc5b9d05
mmm11355 <153916713+mmm11355@users.noreply.github.com>
2026-01-23 14:22:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
   // Относительный базовый путь критически важен для GitHub Pages
-  base: './', 
+  base: '/Miniapp-portf-011/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
